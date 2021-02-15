20e421651bf62a97219fa6909c669cf4912cea52
Andy Sanchez <sanchezzandy206@gmail.com>
2021-02-15 23:03:15 +0000

    Added Sport.java

--- Sport.java
+++ Sport.java
@@ -2,17 +2,17 @@ public abstract class Sport
 {
   private String name;
   private int size;
-  private boolean faul;
+  private boolean isfaul;
   private double points;
 
-  public Sport (String name,int size,boolean faul,double points)
+  public Sport (String name,int size,boolean isfaul,double points)
   {
     this.name = name;
-    this,size = size;
-    this.faul = faul;
-    this,points = points;
+    this.size = size;
+    this.isfaul = isfaul;
+    this.points = points;
   } 
-  
+  //getter and setter for name
   public String getName()
   {
     return name;
@@ -21,4 +21,46 @@ public abstract class Sport
   {
     this.name = name;
   }
+  //getters and setter for size
+  public int getSize()
+  {
+    return size;
+  }
+  public void setSize(int size)
+  {
+    this.size = size;
+  }
+//getters and setter for faul
+  public boolean getFaul()
+  {
+    return isfaul;
+  }
+  public void setFaul(boolean isfaul)
+  {
+    this.isfaul = isfaul;
+  }
+  //getters and setter for points
+  public double getPoints()
+  {
+    return points;
+  }
+  public void setPoints(double points)
+  {
+    this.points = points;
+  }
+  //brain methods
+  public String message()
+  {
+    return name + "is an ok sport for fitness,well not for Andy lol";
+  }
+  public abstract String sound();
+  
+  public String toString()
+  {
+    return "Sport Name:"+name+
+            "\nTeam Size: "+ size+
+            "\nA Faul:"+isfaul+
+            "\nHow many points"+points;
+  }
+
 }
