ad38b7a88e2c0b1fe8e0b8ea5eaa9af5993dbf0a
Andy Sanchez <sanchezzandy206@gmail.com>
2021-02-16 06:49:08 +0000

    Completed Bridge.java

--- Bridge.java
+++ Bridge.java
@@ -1,5 +1,50 @@
 public class Bridge extends Sport
 {
-  private 
+  private String role;
+  private boolean trick;
   private int memory;
+
+  public Lightsaber (String name,String side,int size,boolean isfaul,double points,String role,boolean trick,int memory)
+  {
+    super (name,size,isfaul,points);
+    this.role = role;
+    this.trick = trick;
+    this.memory = memory;
+  }
+  //getter and setter for role
+  public String GetRole()
+  {
+    return role;
+  }
+  public void setRole(String role)
+  {
+    this.role = role;
+  }
+  //getter and setter for trick
+  public String GetTrick()
+  {
+    return trick;
+  }
+  public void setTrick(boolean trick)
+  {
+    this.trick = trick;
+  }
+  //getter and setter for role
+  public String GetMemory()
+  {
+    return memory;
+  }
+  public void setMemory(int memory)
+  {
+    this.memory = memory;
+  }
+  public String toString()
+  {
+    String output = super.toString();
+    output += "\nYour role is: " + role +
+              "\nTricks made: " + trick +
+              "\nYo rememebered: " + memory;
+    return output;
+  }
+
 }
